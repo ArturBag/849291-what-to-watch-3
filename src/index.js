d71a7670e337c0430b42e8936fc78e464a994f56
Артур <artur8888b@gmail.com>
2020-02-10 12:20:41 +0200

    Раздел 1. Часть 2. Добавление компонентов

--- src/index.js
+++ src/index.js
@@ -1,1 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/app/app.jsx";
 
+const MovieHeader = {
+  TITLE: `The Grand Budapest Hotel`,
+  GENRE: `Drama`,
+  ISSUED_DATE: 2014
+};
+
+ReactDOM.render(
+    <App
+      title={MovieHeader.TITLE}
+      genre={MovieHeader.GENRE}
+      issuedDate={MovieHeader.ISSUED_DATE}
+    />,
+    document.querySelector(`#root`)
+);
